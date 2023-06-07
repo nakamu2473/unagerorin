e66172bb10ffc8e0aca8c82a24c4214fcdd9233d
nakamu <oyukita56@gmail.com>
2023-06-07 21:35:42 +0900

    podcastのリンクをaboutに移動

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <iframe allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" frameborder="0" height="175" style="width:100%;max-width:660px;overflow:hidden;border-radius:10px;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.podcasts.apple.com/jp/podcast/1-%E3%82%B7%E3%83%A7%E3%82%A6%E3%82%BF%E3%81%A8%E3%82%AB%E3%83%8D/id1575465539?i=1000528457414"></iframe>
+      
   </div>
 </template>
 
