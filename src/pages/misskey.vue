9ffce2a001ec493891ecb371ef8ee42cfa75b5e1
nakamu <oyukita56@gmail.com>
2023-07-26 22:34:07 +0900

    タイムライン取得と環境変数追加

--- src/pages/misskey.vue
+++ src/pages/misskey.vue
@@ -4,7 +4,8 @@
 export default {
   data() {
     return {
-      mi_i: {}
+      mi_i: {},
+      timeline: {}
     }
   },
   methods: {
@@ -14,12 +15,27 @@ export default {
       const params = {
         method : "POST", 
         headers: {'Content-Type': 'application/json'},
-        body : JSON.stringify({i : "J1whtBfjRS0mvq5jC8razVB7J2JZfq9R"})};
+        body : JSON.stringify({i : import.meta.env.VITE_MISSKEY_TOKEN})};
 
       const response = await fetch(url, params);
       this.mi_i = await response.json();
-      console.log(mi_i);
-    }
+    },
+
+    async getTimeline () {
+      const url = "https://misskey.systems/api/notes/local-timeline";
+      const params = {
+        method : "POST", 
+        headers: {'Content-Type': 'application/json'},
+        body : JSON.stringify(
+          {
+            withFiles: false,
+            limit: 10,
+          }
+        )};
+
+      const response = await fetch(url, params);
+      this.timeline = await response.json();
+    },
   }
 }
 </script>
@@ -29,7 +45,25 @@ export default {
     <div> misskey </div>
     <v-btn @click="iam()">iam</v-btn>
     
-    私は{{ mi_i.name }}です。
-    <img :src="mi_i.avatarUrl" />
+    <p>私は{{ mi_i.name }}です。</p>
+    <p><img :src="mi_i.avatarUrl" /></p>
+    
+    <p><v-btn @click="getTimeline()">Timeline</v-btn></p>
+    <!-- <p><pre> {{timeline }} </pre></p> -->
+
+    <div v-for="(item, key) in timeline" :key="key">
+      <v-avatar>
+        <v-img
+          :width="300"
+          aspect-ratio="16/9"
+          cover
+          :src="item.user.avatarUrl"
+        ></v-img>
+      </v-avatar>
+      <v-list>{{ item.createdAt }}</v-list> 
+      <v-list>{{ item.user.name }}</v-list>
+      <v-list>{{ item.text }}</v-list>
+
+     </div>
   </div>
 </template>
